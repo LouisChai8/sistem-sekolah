bc1bd875f7e67bb25cb5590f53ccc2e44632ea53
Louis Chai <LouisChai8>
2026-04-21 14:19:11 +0700

    feat: implement student create page

--- app/views/students/create.php
+++ app/views/students/create.php
@@ -6,7 +6,7 @@
             </div>
             <!-- Card Header End -->
             <div class="bg-white rounded-lg shadow">
-                <form action="" class="p-4 grid grid-cols-2 gap-4">
+                <form action="/students" method="POST" class="p-4 grid grid-cols-2 gap-4">
                     <div class="space-y-2">
                         <label class="font-bold block" for="name">Nama</label>
                         <input class="px-4 py-2 border rounded-lg w-full" type="text" id="name" name="name"
